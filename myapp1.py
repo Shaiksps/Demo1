f72590edebd19a2853c582f32a2237461050600c
Shaik Suhana <142031092+Shaiksps@users.noreply.github.com>
2024-01-08 21:30:46 +0530

    Create myapp1.py

---
+++ myapp1.py
@@ -0,0 +1,63 @@
+import streamlit as st
+st.set_page_config(page_title='My Favourite')
+st.header("Vehicles")
+col1,col2=st.columns(2)
+with col1:
+  st.subheader("Car1")
+  st.image("./car1.jpg",width=500,use_column_width=True)
+with col2:
+  st.subheader("Car2")
+  st.image("./car2.jpg",width=500,use_column_width=True)
+with col3:
+  st.subheader("Car3")
+  st.image("./car3.jpg",width=500,use_column_width=True)
+with col4:
+  st.subheader("Car4")
+  st.image("./car4.jpg",width=500,use_column_width=True)
+with col5:
+  st.subheader("Car5")
+  st.image("./car5.jpg",width=500,use_column_width=True)
+with col6:
+  st.subheader("Car6")
+  st.image("./car6.jpg",width=500,use_column_width=True)
+with col7:
+  st.subheader("Bike1")
+  st.image("./bike1.jpg",width=500,use_column_width=True)
+with col8:
+  st.subheader("Bike2")
+  st.image("./bike2.jpg",width=500,use_column_width=True)
+with col9:
+  st.subheader("Bike3")
+  st.image("./bike3.jpg",width=500,use_column_width=True)
+with col10:
+  st.subheader("Bike4")
+  st.image("./bike4.jpg",width=500,use_column_width=True)
+with col11:
+  st.subheader("Bike5")
+  st.image("./bike5.jpg",width=500,use_column_width=True)
+with col12:
+  st.subheader("Bike6")
+  st.image("./bike6.jpg",width=500,use_column_width=True)
+with col13:
+  st.subheader("Jeep1")
+  st.image("./jeep1.jpg",width=500,use_column_width=True)
+with col14:
+  st.subheader("Jeep2")
+  st.image("./jeep2.jpg",width=500,use_column_width=True)
+with col15:
+  st.subheader("Jeep3")
+  st.image("./jeep3.jpg",width=500,use_column_width=True)
+with col16:
+  st.subheader("Jeep4")
+  st.image("./jeep4.jpg",width=500,use_column_width=True)
+with col17:
+  st.subheader("Jeep5")
+  st.image("./jeep5.jpg",width=500,use_column_width=True)
+with col18:
+  st.subheader("Jeep6")
+  st.image("./jeep6.jpg",width=500,use_column_width=True)
+  
+  
+  
+  
+  
