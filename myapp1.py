315f15784250809517b1fc7884127180b4cce849
Shaik Suhana <142031092+Shaiksps@users.noreply.github.com>
2024-01-09 11:48:57 +0530

    Update myapp1.py

--- myapp1.py
+++ myapp1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title='My Favourite')
 st.header("Vehicles")
-col1,col2=st.columns(2)
+col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11,col12,col13,col14,col15,col16,col17,col18=st.columns(18)
 with col1:
   st.subheader("Car1")
   st.image("./car1.jpg",width=500,use_column_width=True)
