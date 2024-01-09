75f10688f60146f2f7d603b77b406e62f1dd7904
Shaik Suhana <142031092+Shaiksps@users.noreply.github.com>
2024-01-09 12:16:11 +0530

    Update myapp1.py

--- myapp1.py
+++ myapp1.py
@@ -4,61 +4,42 @@ st.markdown("<h3 style='color:black;font-size:36px'>Types of Vehicles</h3>",unsa
 st.markdown("<h3 style='color: blue;'>Cars</h3>",unsafe_allow_html=True)
 
 st.header("Vehicles")
-col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11,col12,col13,col14,col15,col16,col17,col18=st.columns(18)
-with col1:
-  st.subheader("Car1")
-  st.image("./car1.jpg",width=500,use_column_width=True)
-with col2:
-  st.subheader("Car2")
-  st.image("./car2.jpg",width=500,use_column_width=True)
-with col3:
-  st.subheader("Car3")
-  st.image("./car3.jpg",width=500,use_column_width=True)
-with col4:
-  st.subheader("Car4")
-  st.image("./car4.jpg",width=500,use_column_width=True)
-with col5:
-  st.subheader("Car5")
-  st.image("./car5.jpg",width=500,use_column_width=True)
-with col6:
-  st.subheader("Car6")
-  st.image("./car6.jpg",width=500,use_column_width=True)
-with col7:
-  st.subheader("Bike1")
-  st.image("./bike1.jpg",width=500,use_column_width=True)
-with col8:
-  st.subheader("Bike2")
-  st.image("./bike2.jpg",width=500,use_column_width=True)
-with col9:
-  st.subheader("Bike3")
-  st.image("./bike3.jpg",width=500,use_column_width=True)
-with col10:
-  st.subheader("Bike4")
-  st.image("./bike4.jpg",width=500,use_column_width=True)
-with col11:
-  st.subheader("Bike5")
-  st.image("./bike5.jpg",width=500,use_column_width=True)
-with col12:
-  st.subheader("Bike6")
-  st.image("./bike6.jpg",width=500,use_column_width=True)
-with col13:
-  st.subheader("Jeep1")
-  st.image("./jeep1.jpg",width=500,use_column_width=True)
-with col14:
-  st.subheader("Jeep2")
-  st.image("./jeep2.jpg",width=500,use_column_width=True)
-with col15:
-  st.subheader("Jeep3")
-  st.image("./jeep3.jpg",width=500,use_column_width=True)
-with col16:
-  st.subheader("Jeep4")
-  st.image("./jeep4.jpg",width=500,use_column_width=True)
-with col17:
-  st.subheader("Jeep5")
-  st.image("./jeep5.jpg",width=500,use_column_width=True)
-with col18:
-  st.subheader("Jeep6")
-  st.image("./jeep6.jpg",width=500,use_column_width=True)
+st.subheader("Car1")
+st.image("./car1.jpg",width=500,use_column_width=True)
+st.subheader("Car2")
+st.image("./car2.jpg",width=500,use_column_width=True)
+st.subheader("Car3")
+st.image("./car3.jpg",width=500,use_column_width=True)
+st.subheader("Car4")
+st.image("./car4.jpg",width=500,use_column_width=True)
+st.subheader("Car5")
+st.image("./car5.jpg",width=500,use_column_width=True)
+st.subheader("Car6")
+st.image("./car6.jpg",width=500,use_column_width=True)
+st.subheader("Bike1")
+st.image("./bike1.jpg",width=500,use_column_width=True)
+st.subheader("Bike2")
+st.image("./bike2.jpg",width=500,use_column_width=True)
+st.subheader("Bike3")
+st.image("./bike3.jpg",width=500,use_column_width=True)
+st.subheader("Bike4")
+st.image("./bike4.jpg",width=500,use_column_width=True)
+st.subheader("Bike5")
+st.image("./bike5.jpg",width=500,use_column_width=True)
+st.subheader("Bike6")
+st.image("./bike6.jpg",width=500,use_column_width=True)
+st.subheader("Jeep1")
+st.image("./jeep1.jpg",width=500,use_column_width=True)
+st.subheader("Jeep2")
+st.image("./jeep2.jpg",width=500,use_column_width=True)
+st.subheader("Jeep3")
+st.image("./jeep3.jpg",width=500,use_column_width=True)
+st.subheader("Jeep4")
+st.image("./jeep4.jpg",width=500,use_column_width=True)
+st.subheader("Jeep5")
+st.image("./jeep5.jpg",width=500,use_column_width=True)
+st.subheader("Jeep6")
+st.image("./jeep6.jpg",width=500,use_column_width=True)
   
   
   
